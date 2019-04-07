279c5fddbd463077a680c309b5abba5ffb64b8f3
Osanda Wedamulla <osanda1989@gmail.com>
2019-04-07 19:16:39 +0530

    Initial commit

---
+++ src/main/java/com/osanda/roihunter/fbuserdata/FbUserDataApplication.java
@@ -0,0 +1,13 @@
+package com.osanda.roihunter.fbuserdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbUserDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbUserDataApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/osanda/roihunter/fbuserdata/model/BaseModel.java
@@ -0,0 +1,58 @@
+package com.osanda.roihunter.fbuserdata.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Version;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/***
+ * base model to use as the abstract layer of any entity
+ * 
+ * @author Osanda Wedamulla
+ *
+ */
+
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseModel implements Serializable {
+
+	private static final long serialVersionUID = -1413982697301006018L;
+
+	@CreatedBy
+	@JsonIgnore
+	@Column(name = "CREATED_BY", length = 50, updatable = false)
+	private String createdBy;
+
+	@JsonIgnore
+	@CreatedDate
+	@Column(name = "CREATED_DATE")
+	private LocalDateTime createdDate = LocalDateTime.now();
+
+	@JsonIgnore
+	@LastModifiedBy
+	@Column(name = "LAST_MODIFIED_BY", length = 50)
+	private String lastModifiedBy;
+
+	@JsonIgnore
+	@LastModifiedDate
+	@Column(name = "LAST_MODIFIED_DATE")
+	private LocalDateTime lastModifiedDate = LocalDateTime.now();;
+
+	@Version
+	@JsonIgnore
+	private Long version;
+
+}
